f33212ec0256f958fcbd1bf3cc77ecfda6411cea
nongrata2 <94924908+nongrata2@users.noreply.github.com>
2024-09-27 09:37:25 +0300

    Update 24.sql

--- 24.sql
+++ 24.sql
@@ -1,3 +1,6 @@
+-- https://www.sql-ex.ru/learn_exercises.php?LN=24
+-- Перечислите номера моделей любых типов, имеющих самую высокую цену по всей имеющейся в базе данных продукции.
+
 WITH all_prices AS (SELECT Product.model, price
 FROM Product 
 JOIN PC ON Product.model = PC.model 
