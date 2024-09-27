feabc12dad6bcb676b04e4db0fb0e3abdb89c972
nongrata2 <94924908+nongrata2@users.noreply.github.com>
2024-09-27 09:38:45 +0300

    Update 6.sql

--- 6.sql
+++ 6.sql
@@ -1,3 +1,6 @@
+-- https://www.sql-ex.ru/learn_exercises.php?LN=6
+-- Для каждого производителя, выпускающего ПК-блокноты c объёмом жесткого диска не менее 10 Гбайт, найти скорости таких ПК-блокнотов. Вывод: производитель, скорость.
+
 SELECT DISTINCT maker, speed FROM Product
 JOIN Laptop ON Product.model = Laptop.model
 WHERE hd >= 10
