c8c7abf33dc5b167d110347558cdd8c3c4adea08
nongrata2 <94924908+nongrata2@users.noreply.github.com>
2024-09-27 09:37:58 +0300

    Update 18.sql

--- 18.sql
+++ 18.sql
@@ -1,3 +1,6 @@
+-- https://www.sql-ex.ru/learn_exercises.php?LN=18
+-- Найдите производителей самых дешевых цветных принтеров. Вывести: maker, price
+
 SELECT DISTINCT maker, price FROM (Product
 JOIN Printer ON Printer.model = Product.model
 ) WHERE price = (SELECT MIN(price) FROM Printer WHERE color = 'y') AND color = 'y'
