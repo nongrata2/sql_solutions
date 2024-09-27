ff69a4e60798cb4d0bb72403c08300bf416c7d01
nongrata2 <94924908+nongrata2@users.noreply.github.com>
2024-09-27 11:41:15 +0300

    Update 102.sql

--- 102.sql
+++ 102.sql
@@ -1,23 +1,33 @@
+-- https://www.sql-ex.ru/learn_exercises.php?LN=102
 -- Определить имена разных пассажиров, которые летали только между двумя городами (туда и/или обратно).
 
+SELECT name FROM (SELECT 
+    p.ID_psg,
+    p.name,
+    COUNT(DISTINCT city) AS unique_cities_count
+FROM (
+    SELECT 
+        pit.ID_psg, 
+        t.town_from AS city
+    FROM 
+        pass_in_trip pit
+    JOIN 
+        trip t ON pit.trip_no = t.trip_no
 
-WITH all_prices AS (
-  SELECT Product.model, price, maker
-  FROM Product
-  JOIN PC ON Product.model = PC.model
+    UNION
 
-  UNION ALL
-  
-  SELECT Product.model, price, maker
-  FROM Product
-  JOIN Laptop ON Product.model = Laptop.model
-  
-  UNION ALL
-  
-  SELECT Product.model, price, maker
-  FROM Product
-  JOIN Printer ON Product.model = Printer.model
-  )
-  
-SELECT DISTINCT model, price FROM all_prices
-WHERE maker = 'B'
+    SELECT 
+        pit.ID_psg,
+        t.town_to AS city
+    FROM 
+        pass_in_trip pit
+    JOIN 
+        trip t ON pit.trip_no = t.trip_no
+) AS combined
+JOIN 
+    passenger p ON combined.ID_psg = p.ID_psg
+GROUP BY 
+    p.ID_psg, 
+    p.name
+) AS unique_count
+WHERE unique_cities_count < 3
